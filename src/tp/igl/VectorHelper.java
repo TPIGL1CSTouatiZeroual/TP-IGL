df1ddf4793dc8b0e2c8424487d2e1c37d5d99128
chahrazed <fc_touati@esi.dz>
2017-10-19 19:37:58 +0200

    ajout des commentaires dans la classe VectorHelper pour generer la documentation avec javaDoc.

--- src/tp/igl/VectorHelper.java
+++ src/tp/igl/VectorHelper.java
@@ -2,18 +2,28 @@
 
 package tp.igl;
 
-
+/**
+ *
+ * @author Touati_Zeroual
+ */
 public class VectorHelper  {
    
     private int[] tab ;
     private int min;
     private int max;
 
+    /**
+     * constructeur vide
+     */
     public VectorHelper()
     {
         
     }
 
+    /**
+     * Constructeur qui initialise les attributs 
+     * @param t c'est un tableau avec lequel on initialise l'attribut tab
+     */
     public VectorHelper(int [] t)
     {
         tab = t;
@@ -21,16 +31,25 @@ public class VectorHelper  {
            
     }
  
+    /**
+     * getter qui retourne l'attribut  min
+     * @return le plus petit element du vecteur tab  (entier) 
+     */
     public int getMin() {
         return min;
     }
 
-   
+    /**
+     *getter qui retourne l'attribut max
+     * @return le plus grand element du vecteur tab (entier)
+     */
     public int getMax() {
         return max;
     }
     
-    
+    /**
+     * la methode qui calcule au meme temps le minimum et le maximum du vecteur tab et initialise les attributs min et max 
+     */
     public void max_min()
      {
          min = tab[0] ;
@@ -41,7 +60,12 @@ public class VectorHelper  {
          }
      }
     
-     public void sommer (int [] tab2) throws VectorHelperException
+    /**
+     * une methode qui fait la somme entre deux vecteurs
+     * @param tab2 le vecteur qu'on ajoute à l'attribut tab (on somme les deux vecteur et le resltat sera dans tab) 
+     * @throws VectorHelperException exception lancé si les deux vecterus sont de tailles differentes
+     */
+    public void sommer (int [] tab2) throws VectorHelperException
      {
          if (tab2.length != tab.length ) throw new VectorHelperException ("Veuillez vérifier que les deux tableaux ont la meme longueur ");
          else 
@@ -50,6 +74,9 @@ public class VectorHelper  {
          }
      }
       
+    /**
+     * une methode qui incremente tout les elements du vecteur tab
+     */
     public void formule () // rajoute 1 à tous les éléments 
     {
         for (int i=0;i<tab.length;i++)
@@ -58,7 +85,10 @@ public class VectorHelper  {
         }
     }
     
-     public void inverser()
+    /**
+     * methode qui inverse les elements du vecteur tab
+     */
+    public void inverser()
      {
          int i =0,j=tab.length-1,k=0;
          while (i<j)
@@ -70,6 +100,9 @@ public class VectorHelper  {
          }
      }
      
+    /**
+     * une methode qui trie les elements du vecteur tab
+     */
     public void trier() // tri par bulles
     {
         int i,j,k;
