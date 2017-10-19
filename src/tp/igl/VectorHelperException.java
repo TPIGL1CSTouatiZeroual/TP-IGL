01e2d8b943392bc30c396a17447270d856fd203f
chahrazed <fc_touati@esi.dz>
2017-10-19 19:48:00 +0200

    ajout des commentaires au niveau de la classe VectorHelperException

--- src/tp/igl/VectorHelperException.java
+++ src/tp/igl/VectorHelperException.java
@@ -1,16 +1,22 @@
 
 package tp.igl;
 
-
+/**
+ * classe de gestion d'exception relative a la classe VectorHelper
+ * @author Touati_Zeroual
+ */
 public class VectorHelperException extends Exception {
 
-   
+    /**
+     * Constructeur vide
+     */
     public VectorHelperException() {
     }
 
-    
-    
-    
+    /**
+     * Constructeur qui affiche un message 
+     * @param msg un message qui sera afficher quand une exception est levée par la methode sommer de la classe VectoHelper
+     */
     public VectorHelperException(String msg) {
         super(msg);
     }
