bd8ea0b296714bed317bd3a6d1d05874a3128214
chahrazed <fc_touati@esi.dz>
2017-10-19 19:49:58 +0200

    modification dans la methode start de la classe TPIGL

--- src/tp/igl/TPIGL.java
+++ src/tp/igl/TPIGL.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tp.igl;
 
 import javafx.application.Application;
@@ -14,31 +10,14 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
- *
- * @author acer
+ * 
+ * @author Touati_Zeroual
  */
 public class TPIGL extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     /**
